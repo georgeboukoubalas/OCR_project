7165b04095ae5c6f0f3b46fbc7ec1b067123779d
georgeboukoubalas <47902172+georgeboukoubalas@users.noreply.github.com>
2025-04-19 13:55:42 +0300

    innitial commit FileManager, move txt into folder

--- Extractor.py
+++ Extractor.py
@@ -1,10 +1,10 @@
+import pathlib
 from pdf2image import convert_from_path
 import pytesseract
 import os
 import chardet
-import fitz  # PyMuPDF
-'''import sys
-sys.stdout.reconfigure(encoding='utf-8')'''
+import fitz # PyMuPDF
+from FileManager import move
 
 def detect_pdf_encoding(pdf_path):
     # open pdf
@@ -23,34 +23,44 @@ def detect_pdf_encoding(pdf_path):
     encoding_detected = chardet.detect(extracted_text.encode())['encoding']
     return encoding_detected or "unable to find encoding."
 
-#Find tesseract.exe path
-pytesseract.pytesseract.tesseract_cmd = r"C:\Users\georg\Downloads\My programs-project\llm data analysis project\tesseract.exe"
+def main():
+    #Find tesseract.exe path
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Users\georg\Downloads\My programs-project\llm data analysis project\tesseract.exe"
 
-#Enter PDF path that you want  to copy
-PDFpath = str(input(r"Enter PDF file path: "))
+    #Enter PDF path that you want  to copy
+    pdf_path = input(r"Enter PDF file path: ")
 
-#check if file was found in path
-if not os.path.exists(PDFpath):
-    print(f"file not found: {PDFpath}")
-    print("You may write the file path uncorrectly.")
-    exit()
-else:
-    print(f"file founded successfully")
-    encoding = str(detect_pdf_encoding(PDFpath))
-    #print("🔍encoding detection: " + encoding)
+    #check if file was found in path
+    if not os.path.exists(pdf_path):
+        print(f"file not found: {pdf_path}")
+        print("You may write the file path incorrectly.")
+        exit()
+    else:
+        print(f"file founded successfully")
+        encoding = str(detect_pdf_encoding(pdf_path))
+        #print("🔍encoding detection: " + encoding)
 
-#Enter the file name that final text will be wrote down
-fileName = str(input("Enter file name: "))
+    #Enter the file name that final text will be wrote down
+    file_name = input("Enter a file name: ")
 
-# Convert PDF to a list of images
-pages = convert_from_path(PDFpath, 300)
+    filename_path = r"C:\Users\georg\PycharmProjects\OCR_project\\" + file_name + ".txt"
+    destination_dir_path = r"C:\Users\georg\PycharmProjects\OCR_project\Extracted_txts\\"
 
-#Open file for writing
-with open(fileName + ".txt", "w", encoding=encoding, errors="replace") as file:
-# Extract word from every page
-    for page_num, img in enumerate(pages):
-        text = pytesseract.image_to_string(img, lang='eng')
-        #erase the empty lines
-        text = "\n".join([line for line in text.split("\n") if line.strip()])
-        file.write(f'--- Page {page_num + 1} --- \n')
-        file.write(text)
+    # Convert PDF to a list of images
+    pages = convert_from_path(pdf_path, 300)
+
+    #Open file for writing
+    with open(file_name + ".txt", "w", encoding=encoding, errors="replace") as file:
+    # Extract word from every page
+        for page_num, img in enumerate(pages):
+            text = pytesseract.image_to_string(img, lang='eng')
+            #erase the empty lines
+            text = "\n".join([line for line in text.split("\n") if line.strip()])
+            file.write(f'\n--- Page {page_num + 1} --- \n')
+            file.write(text)
+
+    move(filename_path, destination_dir_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ FileManager.py
@@ -0,0 +1,74 @@
+import os
+import shutil
+
+
+def main():
+    destination_dir_path = folder_creator()
+    choice = input("Would you like to (M)ove or (R)estore?: ")
+
+    if choice.upper() == 'M':
+        file_path = input("Enter the exact path of the file that you want to move: ")
+        move(file_path, destination_dir_path)
+        print("Done.")
+
+    elif choice.upper() == 'R':
+        restore()
+        print("Done.")
+
+    else:
+        print("No valid option selected, closing...")
+
+
+def move(file_path, destination_dir):
+    # Έλεγχος αν το αρχείο υπάρχει
+    if not os.path.isfile(file_path):
+        print(f"❌ File not found: {file_path}")
+        return
+
+    """# Δημιουργία του φακέλου προορισμού αν δεν υπάρχει
+    if not os.path.isdir(destination_dir):
+        try:
+            os.makedirs(destination_dir)
+            print(f"📁 Created destination directory: {destination_dir}")
+        except Exception as e:
+            print(f"❌ Failed to create directory: {e}")
+            return"""
+
+    # Απόσπαση του ονόματος του αρχείου από το πλήρες path
+    file_name = os.path.basename(file_path)
+
+    # Δημιουργία του πλήρους path προορισμού
+    destination_path = os.path.join(destination_dir, file_name)
+
+    # Μετακίνηση του αρχείου
+    try:
+        shutil.move(file_path, destination_path)
+        print(f"✅ File moved successfully to: {destination_path}")
+    except Exception as e:
+        print(f"❌ Error while moving file: {e}")
+
+def restore():
+    with open('quar_id.txt') as f:
+        quar_id = f.readline().strip()
+
+    files = os.listdir(os.getcwd())
+    if files:
+        file = files[0]  # Παίρνουμε το πρώτο αρχείο που βρίσκουμε στον τρέχοντα φάκελο
+        shutil.move(file, os.path.join(quar_id, file))
+    else:
+        print("No files to restore.")
+
+def folder_creator():
+    dir_path = input("Enter the destination path of the folder that file gonna move: ")
+
+    if os.path.isdir(dir_path):
+        print("Directory already exists.")
+    else:
+        os.makedirs(dir_path)
+        print("Directory created successfully!")
+
+    return dir_path
+
+
+if __name__ == '__main__':
+    main()
